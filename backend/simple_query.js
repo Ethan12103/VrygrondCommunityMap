98e4c1ef4491843de03967c65e5391263ca42b28
Bailey Ballard <87134554+bballardUF@users.noreply.github.com>
2023-07-19 17:15:02 +0200

    Update simple_query.js

--- backend/simple_query.js
+++ backend/simple_query.js
@@ -1,23 +1,34 @@
-// Name: Bailey Ballard
-// University of Florida
-// Date: 7/11/2023
-// Description: A javascript file using Node, Express, and MongoDB Atlas
-// Connects to a MongoDB Atlas cluster, defines a search query, returns all of the
-// mathcing organization names as an object, prints it to the corresponding port
-// Additionally it creates a JSON file of all of the organization information
-// See results printed to http://localhost:8000/
+/*
+    Name: Bailey Ballard
+    University of Florida
+    Date: 7/11/2023
+
+    Description: This file has two functions
+
+    1. run()
+    Commented out for the moment
+    Connects to a MongoDB Atlas cluster, defines a search query, returns all of the
+    Mathcing organization names as an object, prints it to the corresponding port
+    Default seach is "school" <-- See line 35
+
+    See results printed to http://localhost:8000/
+
+    2. write_JSON()
+    Connects to a MongoDB Atlas Cluster, defines a blank seach query, writes the entire database
+    to a JSON file in frontend/src/components called FullOrgList.json
+
+*/
 
 const { MongoClient } = require("mongodb");
 const express = require("express");
 const app = express();
 const fs = require('fs');
-const cors = require('cors');
-app.use(cors());
 
 // Connect to your Atlas clusterv
 const uri = "mongodb+srv://VrygrondTrust:ButterflyArtsProject@vrygrondcommunity.donyn7r.mongodb.net/";
 const client = new MongoClient(uri);
 
+/*
 async function run() {
     try {
         // Set namespace
@@ -26,13 +37,9 @@ async function run() {
         // Define pipeline
         const agg = [
             // The string next to "query" needs to be input from the user
-            { $search: { text: { query: "school", path: ["Name", "Services"] } } },
-            {
-                $project: {
-                    _id: 0, Name: 1, Services: 1, "Address 1": 1, "Address 2": 1, "Contact Number 1": 1,
-                    "Contact Number 2": 1, "Contact Persons": 1, "Email Address 1": 1, "Email Address 2": 1, Website: 1
-                }
-            }
+            {$search: { text: {query: "school", path: ["Name", "Services"] }}},
+            {$project: {_id: 0, Name: 1, "Address 1": 1, "Address 2": 1, "Contact Number 1": 1, 
+            "Contact Number 2": 1, "Contact Persons": 1, "Email Address 1": 1, "Email Address 2": 1, Website: 1}}
         ];
 
         // Run pipeline
@@ -62,41 +69,42 @@ async function run() {
         else {
             console.log("NONE");
         }
-
+        
         app.get("/", (req, res) => {
             res.send(results);
         });
-
+        
         const PORT = process.env.PORT || 8000;
-
+        
         app.listen(PORT, console.log(`Server started on port ${PORT}`));
-
+        
     } finally {
         await client.close();
         console.log("Connection Closed");
-    }
+    }  
 
 }
+*/
 
-async function write_JSON() {
+async function write_JSON () {
     await client.connect();
 
     console.log("Connected successfully to server");
     const db = client.db("OrganizationList");
 
-    const query = {};  // A blank query returns all of the collections
-
+    const query = { };  // A blank query returns all of the collections
+    
     const db_array = await db.collection("Information").find(query).toArray();
-
+    
     // Write to file
     try {
-        fs.writeFileSync("../frontend/src/components/Full_Org_List.json", JSON.stringify(db_array, null, 2));
+        fs.writeFileSync("../frontend/src/components/FullOrgList.json", JSON.stringify(db_array));
         console.log("Done writing to file");
     }
-    catch (err) {
+    catch(err) {
         console.log("Error writing to file", err)
     }
 };
 
 write_JSON();
-run().catch(console.dir);
+//run().catch(console.dir);
