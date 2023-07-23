21f619c7018b13b77b8176434a202a24f71a6316
Ethan <efan@ufl.edu>
2023-07-24 01:15:37 +0200

    changes map center when pin moves

--- frontend/src/components/Leaflet.tsx
+++ frontend/src/components/Leaflet.tsx
@@ -4,16 +4,28 @@ import {
   TileLayer,
   Marker,
   Popup,
+  useMap
 } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import osm from './osm-providers';
 import L from 'leaflet';
 
-const center: [number, number] = [-34.083, 18.488];
+//const center: [number, number] = [-34.083, 18.488];
 const markerIcon = new L.Icon({
   iconUrl: require('./images/marker.png'),
   iconSize: [22, 35],
 });
+
+type ChangeViewProps = {
+  center: [number, number];
+  zoom: number;
+};
+
+const ChangeView: React.FC<ChangeViewProps> = ({ center, zoom }) => {
+  const map = useMap();
+  map.flyTo(center, zoom);
+  return null;
+}
 
 interface LeafletMapProps {
   pinLocation: [number, number];
@@ -22,7 +34,7 @@ interface LeafletMapProps {
 const LeafletMap: React.FC<LeafletMapProps> = ({ pinLocation }) => {
   return (
     <MapContainer
-      center={center}
+      center={pinLocation}
       zoom={16}
       style={{
         position: 'absolute',
@@ -33,9 +45,10 @@ const LeafletMap: React.FC<LeafletMapProps> = ({ pinLocation }) => {
         zIndex: 0
       }}
     >
+      <ChangeView center={pinLocation} zoom={16} />
       <TileLayer url={osm.maptiler.url} attribution={osm.maptiler.attribution} />
       <Marker position={pinLocation} icon={markerIcon}>
-        <Popup>Vrygrond Community Center</Popup>
+        <Popup>Your Search</Popup>
       </Marker>
     </MapContainer>
   );
