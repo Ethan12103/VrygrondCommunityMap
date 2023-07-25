78b659ed989096d222e68c47892c67522eea6acb
ㅤ <zhao.yijia@ufl.edu>
2023-07-25 17:38:01 +0200

    windows scripts

---
+++ install.bat
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Installing backend dependencies..."
+cd backend
+npm install
+cd ..
+echo "Installing frontend dependencies..."
+cd frontend
+npm install
+cd ..
+echo "Installation completed."
